0155b54e6d6f970f0ad3e399f0ed3280e5bd1002
Rafał Chojnacki <rafalchojnacki0502@gmail.com>
2022-01-06 15:35:40 +0100

    JsonStory function creation

--- cmd/cyoa/main.go
+++ cmd/cyoa/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"fmt"
 	"os"
@@ -18,9 +17,9 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	d := json.NewDecoder(f)
-	var story cyoa.Story
-	if err := d.Decode(&story); err != nil {
+
+	story, err := cyoa.JsonStory(f)
+	if err != nil {
 		panic(err)
 	}
 	fmt.Printf("%+v\n", story)
